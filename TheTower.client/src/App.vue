db4422860a627f3200e0c139ad8ede0a526922fd
Madison Yarnall <maddycodes01@gmail.com>
2022-01-31 12:13:51 -0700

    push full

---
+++ TheTower.client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer>
+    <div class="text-light text-center p-4 dafooter">©Tower Inc. 2022</div>
+  </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+export default {
+  name: 'App',
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+.dafooter {
+  background-color: #303030;
+}
+
+main {
+  background-color: #303030;
+}
+</style>
