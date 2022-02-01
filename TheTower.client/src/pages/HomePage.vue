d4ca9a2e533029e2b98b42c76e36402bbb26fc70
Madison Yarnall <maddycodes01@gmail.com>
2022-01-31 19:55:36 -0700

    Everything broke. lovely.

--- TheTower.client/src/pages/HomePage.vue
+++ TheTower.client/src/pages/HomePage.vue
@@ -33,27 +33,9 @@
       <div class="col-md-3 mt-3 mx-4">
         <div class="card selectable eventcard">
           <div class="text-block">
-            <h4 class="text-center">The Event</h4>
-            <p class="px-2">March 11th, 2022 at the Ford Idaho Center</p>
-            <p class="text-end px-2">200 Spots Left</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3 mt-3 mx-4">
-        <div class="card selectable eventcard2">
-          <div class="text-block">
-            <h4 class="text-center">The Event</h4>
-            <p class="px-2">March 11th, 2022 at the Ford Idaho Center</p>
-            <p class="text-end px-2">200 Spots Left</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3 mt-3 mx-4">
-        <div class="card selectable eventcard4">
-          <div class="text-block">
-            <h4 class="text-center">The Event</h4>
-            <p class="px-2">March 11th, 2022 at the Ford Idaho Center</p>
-            <p class="text-end px-2">200 Spots Left</p>
+            <h4 class="text-center">THE EVENT</h4>
+            <p class="px-2">March 11 at the Ford Idaho Center</p>
+            <p class="text-end px-2">200 Spots Remaining</p>
           </div>
         </div>
       </div>
@@ -108,34 +90,6 @@ export default {
 }
 
 .eventcard:hover {
-  transition: all 0.2s ease-in-out;
-  transform: scale(1.1);
-}
-
-.eventcard2 {
-  height: 30vh;
-  width: 40vh;
-  background-image: url("https://images.unsplash.com/photo-1506157786151-b8491531f063?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80");
-  background-size: cover;
-  border-color: #444444;
-  border-width: 4px;
-}
-
-.eventcard2:hover {
-  transition: all 0.2s ease-in-out;
-  transform: scale(1.1);
-}
-
-.eventcard4 {
-  height: 30vh;
-  width: 40vh;
-  background-image: url("https://images.unsplash.com/photo-1550184658-ff6132a71714?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1389&q=80");
-  background-size: cover;
-  border-color: #444444;
-  border-width: 4px;
-}
-
-.eventcard4:hover {
   transition: all 0.2s ease-in-out;
   transform: scale(1.1);
 }
--- TheTower/server/controllers/CommentsController.js
+++ TheTower/server/controllers/CommentsController.js
@@ -12,7 +12,6 @@ export class CommentsController extends BaseController {
       .delete('/:commentId', this.remove)
   }
 
-  //hello
   async create(req, res, next) {
     try {
       req.body.creatorId = req.userInfo.id
